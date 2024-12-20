3ae7db71965abdcbe2945024148237151e741c45
JChBrandt <59009010+JChBrandt@users.noreply.github.com>
2024-12-20 17:53:46 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -15,9 +15,9 @@ sentiment_analyzer_url = os.getenv(
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params=params + key + "=" + value +"&"
 
     request_url = backend_url+endpoint+"?"+params
 
