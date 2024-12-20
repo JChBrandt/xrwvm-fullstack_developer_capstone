88110b879ad1827ae0ebde6b3b8c29907833871e
JChBrandt <59009010+JChBrandt@users.noreply.github.com>
2024-12-20 18:34:35 +0100

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
@@ -20,96 +21,96 @@ def initiate():
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
-      {
-          "name": "Pathfinder",
-          "modeltype": "SUV",
-          "year": 2023,
-          "maker": car_make_instances[0]
-      },
-      {
-          "name": "Qashqai",
-          "modeltype": "SUV",
-          "year": 2023,
-          "maker": car_make_instances[0]
-      },
-      {
-          "name": "XTRAIL",
-          "modeltype": "SUV",
-          "year": 2023,
-          "maker": car_make_instances[0]
-      },
-      {
-          "name": "A-Class",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[1]
-      },
-      {
-          "name": "C-Class",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[1]
-      },
-      {
-          "name": "E-Class",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[1]
-      },
-      {
-          "name": "A4",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[2]
-      },
-      {
-          "name": "A5",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[2]
-      },
-      {
-          "name": "A6",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[2]
-      },
-      {
-          "name": "Sorrento",
-          "modeltype": "SUV",
-          "year": 2023,
-          "maker": car_make_instances[3]
-      },
-      {
-          "name": "Carnival",
-          "modeltype": "SUV",
-          "year": 2023,
-          "maker": car_make_instances[3]
-      },
-      {
-          "name": "Cerato",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[3]
-      },
-      {
-          "name": "Corolla",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[4]
-      },
-      {
-          "name": "Camry",
-          "modeltype": "Sedan",
-          "year": 2023,
-          "maker": car_make_instances[4]
-      },
-      {
-          "name": "Kluger",
-          "modeltype": "SUV",
-          "year": 2023,
-          "maker": car_make_instances[4]
-      },
+        {
+            "name": "Pathfinder",
+            "modeltype": "SUV",
+            "year": 2023,
+            "maker": car_make_instances[0]
+        },
+        {
+            "name": "Qashqai",
+            "modeltype": "SUV",
+            "year": 2023,
+            "maker": car_make_instances[0]
+        },
+        {
+            "name": "XTRAIL",
+            "modeltype": "SUV",
+            "year": 2023,
+            "maker": car_make_instances[0]
+        },
+        {
+            "name": "A-Class",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[1]
+        },
+        {
+            "name": "C-Class",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[1]
+        },
+        {
+            "name": "E-Class",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[1]
+        },
+        {
+            "name": "A4",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[2]
+        },
+        {
+            "name": "A5",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[2]
+        },
+        {
+            "name": "A6",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[2]
+        },
+        {
+            "name": "Sorrento",
+            "modeltype": "SUV",
+            "year": 2023,
+            "maker": car_make_instances[3]
+        },
+        {
+            "name": "Carnival",
+            "modeltype": "SUV",
+            "year": 2023,
+            "maker": car_make_instances[3]
+        },
+        {
+            "name": "Cerato",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[3]
+        },
+        {
+            "name": "Corolla",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[4]
+        },
+        {
+            "name": "Camry",
+            "modeltype": "Sedan",
+            "year": 2023,
+            "maker": car_make_instances[4]
+        },
+        {
+            "name": "Kluger",
+            "modeltype": "SUV",
+            "year": 2023,
+            "maker": car_make_instances[4]
+        },
         # Add more CarModel instances as needed
     ]
 
